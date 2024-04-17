eaca43d69eb772e432748b23990878380aa58534
Lucas <lucas04@foxmail.com>
2024-04-17 11:35:53 +0800

    Add download scripts

--- bwa/initialize.sh
+++ bwa/initialize.sh
@@ -11,9 +11,42 @@ fi
 if conda env list | grep -q "bwa"
 then
     echo "Great! The environment already exists."
+    # Activate the environment
+    echo "Activating the environment..."
+    conda activate bwa
 else
     echo "Creating the environment..."
     conda env create -f requirements.txt
     echo "The environment has been created."
 fi
 
+echo "Initialization is complete."
+
+# Set the path to the genome data
+echo "Please enter the path to the genome data:"
+read genome_path
+# The path of the genome data is '/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host'
+export GENOME_PATH=$genome_path
+echo "The path to the genome data has been set to $GENOME_PATH."
+
+# Set the path to the indexing data
+echo "Please enter the path to the indexing data:"
+read indexing_path
+
+export INDEXING_PATH=$indexing_path
+echo "The path to the indexing data has been set to $INDEXING_PATH."
+
+
+# Indexing
+bwa index -a bwtsw 
+
+
+# 
+echo "Indexing the genome data..."
+for file_fq1 in $(ls *_1.fastq.gz)
+do
+    file_name=$(basename "$file_fq1")
+    sample_name="${file_name%%_1*}"
+    file_fq2="${sample_name}_2.fastq.gz"
+    bwa index -p $sample_name $GENOME_PATH/$sample_name.fasta
+done
