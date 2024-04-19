8d326f9a19245b7d33da2534e06b89838c556e6a
Lucas <lucas04@foxmail.com>
2024-04-19 18:20:59 +0800

    Update index file

--- bwa/download_index.sh
+++ bwa/download_index.sh
@@ -8,16 +8,6 @@ read download_dir
 # Currently, the download directory is '/mnt/raid6/bacphagenetwork/data/bwa_index'
 cd $download_dir
 
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/b37_human_g1k_v37.fasta
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/b37_1000G_phase1.indels.b37.vcf
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/b37_Mills_and_1000G_gold_standard.indels.b37.vcf
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/b37_dbsnp_138.b37.vcf.zip
-unzip b37_dbsnp_138.b37.vcf.zip
-
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/gatk-examples_example1_NA20318_ERR250971_1.filt.fastq.gz
-gunzip gatk-examples_example1_NA20318_ERR250971_1.filt.fastq.gz
-
-wget -c https://public-ehpc-package.oss-cn-hangzhou.aliyuncs.com/lifescience/gatk-examples_example1_NA20318_ERR250971_2.filt.fastq.gz
-gunzip gatk-examples_example1_NA20318_ERR250971_2.filt.fastq.gz
+wget -c https://processing.open-genomes.org/reference/CP086569.1-CHM13/CHM13_v1.1.fa
 
 echo "The genome data has been downloaded."
