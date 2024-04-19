7e853e27e2adb2cde74129942e1964466871a0f5
Lucas <lucas04@foxmail.com>
2024-04-19 20:38:19 +0800

    Update conda initialization in bwa/slurm/slurm_bwa_process.sh

--- bwa/slurm/slurm_bwa_process.sh
+++ bwa/slurm/slurm_bwa_process.sh
@@ -6,8 +6,6 @@
 #SBATCH --mem=1G
 #SBATCH --export=GENOME_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa',ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis'
 
-source /etc/profile.d/modules.sh
-module load anaconda3
 conda init bash
 source ~/.bashrc
 
