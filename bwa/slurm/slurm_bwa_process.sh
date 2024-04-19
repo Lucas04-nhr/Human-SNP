e7178246aaa5c5d5d10e82151457e79d21a77f40
Lucas <lucas04@foxmail.com>
2024-04-19 20:28:40 +0800

    Update paths in bwa processing and environment setup scripts

--- bwa/slurm/slurm_bwa_process.sh
+++ bwa/slurm/slurm_bwa_process.sh
@@ -5,6 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
 #SBATCH --partition=standard
+#SBATCH --export=GENOME_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa',ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis'
 
 source /etc/profile.d/modules.sh
 module load anaconda3
