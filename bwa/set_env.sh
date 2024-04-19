d6282f3b602a0441b3954f68b2b70fd6c95350ce
Lucas <lucas04@foxmail.com>
2024-04-19 20:19:35 +0800

    Add support for SLURM

---
+++ bwa/set_env.sh
@@ -0,0 +1,45 @@
+# Check whether conda is installed
+if ! command -v conda &> /dev/null
+then
+    echo "Conda could not be found, please install it first."
+    exit
+fi
+
+conda activate base
+# Check whether the environment exists
+if conda env list | grep -q "bwa"
+then
+    echo "Great! The environment already exists."
+    # Activate the environment
+    echo "Activating the environment..."
+    conda activate bwa
+else
+    echo "Creating the environment..."
+    conda env create -n bwa -f requirements.txt
+    echo "The environment has been created, activating it..."
+    conda activate bwa
+fi
+
+echo "Initialization is complete."
+
+# Set the path to the genome data
+echo "Please enter the path to the genome data:"
+read genome_path
+# The path of the genome data is '/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host'
+export GENOME_PATH=$genome_path
+echo "The path to the genome data has been set to $GENOME_PATH."
+
+# Set the path to the indexing data
+echo "Please enter where the the indexing data located:"
+read indexing_path
+
+export INDEXING_PATH=$indexing_path
+# The path to the indexing data is '/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa'
+echo "The path to the indexing data has been set to $INDEXING_PATH."
+
+# Set the path to store analysis results
+echo "Please enter the path to store the analysis results:"
+read analysis_path
+# The path to store the analysis results is '/mnt/raid6/bacphagenetwork/data/bwa_analysis'
+export ANALYSIS_PATH=$analysis_path
+echo "The path to store the analysis results has been set to $ANALYSIS_PATH."
---
+++ bwa/slurm/slurm_bwa_setenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=bwa_analysis
+#SBATCH --output=bwa_analysis.out
+#SBATCH --error=bwa_analysis.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=1G
+#SBATCH --partition=standard
+#SBATCH --export=GENOME_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa',ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis'
+
+source /etc/profile.d/modules.sh
+module load anaconda3
+conda init bash
+source ~/.bashrc
+
+conda activate base
+
+srun bash processing.sh
