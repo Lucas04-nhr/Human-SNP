1fe6b3c58d7f797c67b6c54b45d30d9b37ed02f4
Lucas <lucas04@foxmail.com>
2024-04-19 18:30:28 +0800

    Update download URL

--- bwa/processing.sh
+++ bwa/processing.sh
@@ -36,6 +36,7 @@ echo "Please enter where the the indexing data located:"
 read indexing_path
 
 export INDEXING_PATH=$indexing_path
+# The path to the indexing data is '/mnt/raid6/bacphagenetwork/data/bwa_index'
 echo "The path to the indexing data has been set to $INDEXING_PATH."
 
 # Set the path to store analysis results
