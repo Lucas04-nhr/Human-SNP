6541dec11103afd76cbb58e86548773660278eab
Lucas <lucas04@foxmail.com>
2024-04-24 18:33:22 +0800

    Update log file paths in bwa/slurm/slurm_bwa_process.sh

--- bwa/slurm/slurm_bwa_process.sh
+++ bwa/slurm/slurm_bwa_process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=bwa_analysis
-#SBATCH --output=bwa_analysis.%x.%j.out
-#SBATCH --error=bwa_analysis.%x.%j.err
+#SBATCH --output=./log/bwa_analysis.%x.%j.out
+#SBATCH --error=./log/bwa_analysis.%x.%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
 #SBATCH --export=GENOME_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa',ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis'
