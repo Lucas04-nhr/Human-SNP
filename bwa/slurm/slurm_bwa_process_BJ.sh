c70ad377f6be015f2630a31821b0f236dfe4baa2
Lucas <lucas04@foxmail.com>
2024-04-25 10:15:50 +0800

    Update SLURM output and error paths for Beijing and Guangzhou in bwa/slurm/slurm_bwa_process_BJ.sh and bwa/slurm/slurm_bwa_process_GZ.sh

--- bwa/slurm/slurm_bwa_process_BJ.sh
+++ bwa/slurm/slurm_bwa_process_BJ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=bwa_analysis
-#SBATCH --output=./log/bwa_analysis.%j.out
-#SBATCH --error=./log/bwa_analysis.%j.err
+#SBATCH --output=./log/Beijing/bwa_analysis_beijing.%j.out
+#SBATCH --error=./log/Beijing/bwa_analysis_beijing.%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
 #SBATCH --export=GENOME_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/bwa_index/chm13v2.0_noY.fa',ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis'
