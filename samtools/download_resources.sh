4d1522d7f76b9f174dfd5abacbfc39c665b6e6d9
Lucas <lucas04@foxmail.com>
2024-05-15 16:53:20 +0800

    chore: Update samtools download script to include unzipping step

--- samtools/download_resources.sh
+++ samtools/download_resources.sh
@@ -9,3 +9,7 @@ echo "The working directory has been changed to $PWD."
 # Download the reference genome
 echo "Downloading the reference genome..."
 wget -c https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_reference_assembly_sequence/hs37d5.fa.gz
+
+# Unzip the reference genome
+echo "Unzipping the reference genome..."
+gunzip hs37d5.fa.gz
