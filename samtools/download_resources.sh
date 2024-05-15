d0f570dfa11686481205a411f339e1bba1d630fb
Lucas <lucas04@foxmail.com>
2024-05-15 17:18:26 +0800

    chore: Add scripts to create sbatch index files for Beijing and Guangzhou

--- samtools/download_resources.sh
+++ samtools/download_resources.sh
@@ -13,3 +13,9 @@ wget -c https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/phase2_re
 # Unzip the reference genome
 echo "Unzipping the reference genome..."
 gunzip hs37d5.fa.gz
+if [ $? -eq 0 ]; then
+    echo "The reference genome has been unzipped."
+else
+    echo "Error: The reference genome could not be unzipped."
+    exit 1
+fi
