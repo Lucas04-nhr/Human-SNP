f77f36c7b0097614fdf297d9e9f2d3357d59dca0
Lucas <lucas04@foxmail.com>
2024-05-22 16:49:01 +0800

    chore: Update slurm_samtools_BJ.sh script to convert SAM to BAM and index BAM file

--- samtools/slurm_samtools_GZ.sh
+++ samtools/slurm_samtools_GZ.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=samtools_index_BJ
-#SBATCH --output=./log/Beijing/samtools_BJ_.%j.out
-#SBATCH --error=./log/Beijing/samtools_BJ_.%j.err
+#SBATCH --job-name=samtools_index_bj
+#SBATCH --output=./log/Guangzhou/samtools_GZ.%j.out
+#SBATCH --error=./log/Guangzhou/samtools_GZ.%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
-#SBATCH --export=ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis/Beijing',INDEX_PATH='/mnt/raid6/bacphagenetwork/data/samtools_analysis/Beijing'
-#SBATCH --array=1-201%4
+#SBATCH --export=ANALYSIS_PATH='/mnt/raid6/bacphagenetwork/data/bwa_analysis/Guangzhou',INDEX_PATH='/mnt/raid6/bacphagenetwork/data/samtools_analysis/Guangzhou'
+#SBATCH --array=1-160%4
 
 echo "The working directory has been changed to $ANALYSIS_PATH."
 
 infile=($( cat bj_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
-sample_name=$(echo "$infile" | grep -oE 'BJ[0-9]{3}')
+sample_name=$(echo "$infile" | grep -oE 'GZ[0-9]{3}')
 
 # Convert the SAM file to BAM file
 echo "Processing $sample_name..."
@@ -19,7 +19,7 @@ echo "The path to the SAM file is $ANALYSIS_PATH/${sample_name}.sam."
 echo "The path to the BAM file is $INDEX_PATH/${sample_name}.bam."
 samtools view -bS $ANALYSIS_PATH/${sample_name}.sam > $INDEX_PATH/${sample_name}.bam || { echo "Error: samtools view failed in processing $sample_name."; exit 1; }
 
-echo "The SAM file $sample_name has been successfully converted to BAM file."
+echo "The SAM file has been successfully converted to BAM file."
 
 # Indexing the BAM file
 echo "Indexing the BAM file..."
@@ -27,4 +27,4 @@ echo "The path to the BAM file is $INDEX_PATH/${sample_name}.bam."
 echo "The path to the BAM index file is $INDEX_PATH/${sample_name}.bam.bai."
 samtools index $INDEX_PATH/${sample_name}.bam $INDEX_PATH/${sample_name}.bam.bai || { echo "Error: samtools index failed in processing $sample_name."; exit 1; }
 
-echo "The $sample_name BAM file has been successfully indexed."
+echo "The BAM file has been successfully indexed."
