c11452d369e2437b181788ec53810117af25584e
Lucas <lucas04@foxmail.com>
2024-05-27 18:11:52 +0800

    Update file path

--- samtools/create_sbatch_BJ_01.sh
+++ samtools/create_sbatch_BJ_01.sh
@@ -3,23 +3,23 @@
 # Set the path to the genome data
 echo "Please enter the path to the processed *.sam data:"
 read genome_path
-# The path of the processed *.sam data is '/mnt/raid6/bacphagenetwork/data/bwa_analysis/Guangzhou'
+# The path of the processed *.sam data is '/mnt/raid6/bacphagenetwork/data/bwa_analysis/Beijing'
 export GENOME_PATH=$genome_path
 echo "The path to the genome data has been set to $GENOME_PATH."
 
 # Check whether the index exists
-if [ ! -f "./gz_sbatch.list" ]
+if [ ! -f "./bj_01_sbatch.list" ]
 then
     echo "The index does not exist, creating it ..."
-    touch ./gz_sbatch.list
+    touch ./bj_01_sbatch.list
 else
     echo "The index already exists, do you want to overwrite it? (y/n)"
     read answer
     if [ $answer == "y" ] || [ $answer == "Y" ]
     then
         echo "Overwriting the index ..."
-        rm ./gz_sbatch.list
-        touch ./gz_sbatch.list
+        rm ./bj_01_sbatch.list
+        touch ./bj_01_sbatch.list
     else
         echo "The index will not be overwritten."
         exit
@@ -30,7 +30,7 @@ fi
 echo "Adding the genome data to the config file..."
 for file_fq1 in $(ls ${GENOME_PATH})
 do
-    echo "$GENOME_PATH/$file_fq1" >> ./gz_sbatch.list
+    echo "$GENOME_PATH/$file_fq1" >> ./bj_01_sbatch.list
     echo "The genome data of $sample_name has been added to the config file."
 done
 
