58d63b7ba3732f64eab0a93e88901a97bbf48354
Lucas <lucas04@foxmail.com>
2024-05-30 10:14:30 +0800

    Update slurm_samtools_BJ.sh and slurm_samtools_GZ.sh scripts to fix output file naming issue

---
+++ wes/create_samples_index_BJ.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# Set the path to the genome data
+echo "Please enter the path to the genome data of Beijing:"
+read genome_path
+# The path of the genome data is '/mnt/raid6/bacphagenetwork/data/samtools_reults/Beijing'
+# First check whether the directory exists.
+if [ ! -d $genome_path ]
+then
+    echo "The directory does not exist, please check the path."
+    exit
+fi
+
+# Set the path to the genome data
+export GENOME_PATH=$genome_path
+echo "The path to the genome data has been set to $GENOME_PATH."
+
+# Check whether the index exists
+if [ ! -f "./Beijing/samples.index" ]
+then
+    echo "The index does not exist, creating it ..."
+    if [ -d ./Beijing ]
+    then
+        echo "The directory already exists."
+    else
+        echo "The directory does not exist, creating it ..."
+        mkdir ./Beijing
+    fi
+
+else
+    echo "The index of Beijing already exists, do you want to overwrite it? (y/n)"
+    read answer
+    if [ $answer == "y" ] || [ $answer == "Y" ]
+    then
+        echo "Overwriting the index ..."
+        rm -rf ./Beijing/samples.index
+    else
+        echo "The index will not be overwritten."
+        exit
+    fi
+fi
+
+# Add the genome data to the config file "samples.index"
+echo "Adding the genome data to the config file..."
+for file in $GENOME_PATH/*.bam
+do
+    echo $file >> ./Beijingsamples.index
+done
+
+
+
+# Check if any error occurred during the process
+if [ $? -eq 0 ]; then
+    echo "Success!"
+else
+    echo "Warning: An error occurred during the process."
+    exit 1
+fi
