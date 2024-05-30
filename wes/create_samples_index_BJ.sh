34358a3f52213b934c27254e3e96d4a19ecd7b00
Lucas <lucas04@foxmail.com>
2024-05-30 10:24:21 +0800

    Refactor Beijingsamples.index generation to include sample names

--- wes/create_samples_index_BJ.sh
+++ wes/create_samples_index_BJ.sh
@@ -45,6 +45,7 @@ echo "Adding the genome data to the config file..."
 for file in $GENOME_PATH/*.bam
 do
     file_name=$(basename $file)
+    sample_name=$(echo "$file_name" | grep -oE 'BJ[0-9]{3}')
     echo "$file_name    $file   0" >> ./Beijing/samples.index
 done
 
