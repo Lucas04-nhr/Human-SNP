44d1e577268d5f28b6d29802e7a3ca389d684907
Lucas04-nhr <lucas04@foxmail.com>
2024-07-05 22:54:58 +0800

    Update custom fonts

--- samtools/miscellaneous/data/processing/Beijing.py
+++ samtools/miscellaneous/data/processing/Beijing.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 
+# Set the font properties
+# Specify the path to your custom font file
+font_path = './fonts/Roobert-Regular.ttf'  # Update this path to the actual font file location
+# Create a FontProperties instance with your custom font
+custom_font = FontProperties(fname=font_path, size=12)
+custom_font_label = FontProperties(fname=font_path, size=10)
 
 # Read the data
 data_bj = pd.read_csv('../original/Beijing.txt', sep='\t')
@@ -27,10 +34,12 @@ data_bj_converted['Paired in sequencing'] = data_bj_converted['Paired in sequenc
 plt.figure(figsize=(10, 6), dpi=300)
 plt.plot(data_bj_converted['Mapped'], label='Mapped')
 plt.plot(data_bj_converted['Paired in sequencing'], label='Paired in sequencing')
-plt.xlabel('Sample')
-plt.ylabel('Count/%')
-plt.title('Mapped and Paired in sequencing reads')
-plt.legend()
+plt.xlabel('Sample', fontproperties=custom_font)
+plt.ylabel('Count/%', fontproperties=custom_font)
+plt.title('Mapped and Paired in sequencing reads', fontproperties=custom_font)
+plt.legend(prop=custom_font_label)
+for label in plt.gca().get_xticklabels():
+    label.set_fontproperties(custom_font_label)
 plt.savefig('../results/Beijing.png')
 plt.show()
 
@@ -50,9 +59,11 @@ plt.figure(figsize=(10, 6), dpi=300)
 plt.plot(data_bj_converted['Ratio1'], label='Mapped/Paired in sequencing ratio')
 plt.plot(data_bj_converted['Ratio2'], label='Paired in sequencing/Total ratio')
 plt.plot(data_bj_converted['Ratio3'], label='Mapped/Total ratio')
-plt.xlabel('Sample')
-plt.ylabel('Ratio/%')
-plt.title('Sequence read ratios')
-plt.legend()
+plt.xlabel('Sample', fontproperties=custom_font)
+plt.ylabel('Ratio/%', fontproperties=custom_font)
+plt.title('Sequence read ratios', fontproperties=custom_font)
+plt.legend(prop=custom_font_label)
+for label in plt.gca().get_xticklabels():
+    label.set_fontproperties(custom_font_label)
 plt.savefig('../results/Beijing_ratio.png')
 plt.show()
