8ac3a4be3e0dab937dc1ac146bc7c4cc14f5da71
ErikaSirius <144027085+ErikaSirius@users.noreply.github.com>
2024-07-07 17:17:47 +0800

    modified code

---
+++ samtools/miscellaneous/data/processing/beijing.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+# Read the data
+data_bj = pd.read_csv('D:/pythonCODE/envTest/Beijing.txt', sep='\t')
+
+# Replace ".sorted" with an empty string in the first column
+data_bj.iloc[:, 0] = data_bj.iloc[:, 0].str.replace(".sorted", "", regex=False)
+
+# Display the modified DataFrame to verify changes
+print(data_bj.head())
+
+# Select only the "Sample", "Mapped", and "Paired in sequencing" columns
+data_bj_filtered = data_bj[['Sample', 'Total', 'Mapped', 'Paired in sequencing']]
+
+# Display the filtered DataFrame to verify the selected columns
+print(data_bj_filtered.head())
+
+# Prepare the data for plotting
+data_bj_converted = data_bj_filtered.astype({'Mapped': int, 'Paired in sequencing': int})
+data_bj_converted['Mapped'] = data_bj_converted['Mapped'] / data_bj_converted['Total'] * 100
+data_bj_converted['Paired in sequencing'] = data_bj_converted['Paired in sequencing'] / data_bj_converted['Total'] * 100
+print(data_bj_converted.head())
+
+# Plot the data
+#plt.figure(figsize=(10, 6), dpi=300)
+#plt.plot(data_bj_converted['Mapped'], label='Mapped')
+#plt.plot(data_bj_converted['Paired in sequencing'], label='Paired in sequencing')
+#plt.xlabel('Sample')
+#plt.ylabel('Count/%')
+#plt.title('Mapped and Paired in sequencing reads')
+
+#plt.savefig('D:/pythonCODE/envTest/Beijing.png')
+#plt.show()
+
+# Another Plot
+
+# Calculate the ratio of "Mapped" to "Paired in sequencing" reads
+data_bj_converted['Ratio1'] = data_bj_converted['Mapped'] / data_bj_converted['Paired in sequencing']
+
+# Calculate the ratio of "Paired in sequencing" to "Total" reads
+data_bj_converted['Ratio2'] = data_bj_converted['Paired in sequencing']
+
+# Calculate the ratio of "Mapped" to "Total" reads
+data_bj_converted['Ratio3'] = data_bj_converted['Mapped']
+
+# Plot the ratio
+plt.figure(figsize=(10, 6), dpi=300)
+#plt.plot(data_bj_converted['Ratio1'], label='Paired in sequencing/Mapped ratio')
+plt.plot(data_bj_converted['Ratio2'], label='Paired in sequencing/Total ratio')
+plt.plot(data_bj_converted['Ratio3'], label='Mapped/Total ratio')
+plt.xlabel('Sample')
+plt.ylabel('Ratio/%',)
+plt.title('Sequence read ratios')
+plt.legend(loc='upper right')
+
+plt.savefig('D:/pythonCODE/envTest/Beijing_ratio.png')
+plt.show()
---
+++ samtools/miscellaneous/data/processing/beijingSorted.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+# Read the data
+data_bj = pd.read_csv('D:/pythonCODE/envTest/BeijingSorted.txt', sep='\t')
+
+plt.plot(data_bj['Mapped/Paired in sequencing'], label='Paired in sequencing/Mapped ratio')
+plt.xlabel('Sample')
+plt.ylabel('Ratio')
+plt.title('	Mapped/Paired in sequencing')
+plt.savefig('D:/pythonCODE/envTest/Beijing_pm2.png')
+plt.show()
