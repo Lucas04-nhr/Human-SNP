10db15fa57f65fce8265b46684502c7ca381706f
Lucas04-nhr <lucas04@foxmail.com>
2024-07-28 16:47:17 +0800

    Update sbatch scripts for Beijing and Guangzhou

--- samtools/breakpoints/mark/create_sbatch_GZ.sh
+++ samtools/breakpoints/mark/create_sbatch_GZ.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Set the path to the genome data
-echo "Please enter the path to the pre-processed *.bam data:"
+echo "Please enter the path to the pre-processed *.bam data of Guangzhou:"
 read genome_path
 # The path of the pre-processed *.bam data is '/mnt/raid6/bacphagenetwork/data/02_samtools_analysis/Guangzhou'
 export GENOME_PATH=$genome_path
