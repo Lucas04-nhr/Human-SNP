09373fa91939c87bc791c32a518958792a4c63ba
Lucas04-nhr <lucas04@foxmail.com>
2024-07-30 09:44:16 +0800

    chore: Update samtools fixmate command to use 4 threads for adding the tag

--- samtools/breakpoints/add_tag/processing_GZ.sh
+++ samtools/breakpoints/add_tag/processing_GZ.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=mark_dulplicates_GZ
-#SBATCH --output=./log/Guangzhou/mark_dulplicates_GZ_%j.out
-#SBATCH --error=./log/Guangzhou/mark_dulplicates_GZ_%j.err
+#SBATCH --job-name=add_tag_GZ
+#SBATCH --output=./log/Guangzhou/add_tag_GZ_%j.out
+#SBATCH --error=./log/Guangzhou/add_tag_GZ_%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
-#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/03_samtools_sorted/Guangzhou',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/04_samtools_marked/Guangzhou',STATS_PATH='/mnt/raid6/bacphagenetwork/data/04_samtools_marked_stats',INDEX_FILE='/mnt/raid6/bacphagenetwork/data/00_bwa_index/chm13v2/chm13v2.0_noY.fa'
-#SBATCH --array=1-201%4
+#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/03_samtools_sorted/Guangzhou',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/04_samtools_tag_added/Guangzhou'
+#SBATCH --array=1-160%4
 
 # Initialize the environment
 echo "Initializing..."
@@ -18,7 +18,7 @@ sample_name=$(echo "$infile" | grep -oE 'GZ[0-9]{3}')
 
 # Check all the needed files exist
 
-echo "Checking $INPUT_PATH/${sample_name}_sorted.bam..."
+echo "Checking $INPUT_PATH/${sample_name}.bam..."
 if [ ! -f "$INPUT_PATH/${sample_name}_sorted.bam" ]
 then
     echo "Error: $INPUT_PATH/${sample_name}_sorted.bam does not exist."
@@ -29,10 +29,8 @@ fi
 
 echo "Initializing complete."
 
-# Mark the duplicates
-echo "Marking the duplicates of ${sample_name}..."
+# Add the tag
 
-samtools markdup -r -s $INPUT_PATH/${sample_name}_sorted.bam $OUTPUT_PATH/${sample_name}_marked.bam -f $STATS_PATH/${sample_name}.txt -O --reference $INDEX_FILE --write-index|| { echo "Error: Failed to mark the duplicates of $INPUT_PATH/${sample_name}.bam"; exit 1; }
+samtools fixmate -@ 4 -m $INPUT_PATH/${sample_name}_sorted.bam $OUTPUT_PATH/${sample_name}.bam || { echo "Error: Failed to add the tag of $INPUT_PATH/${sample_name}_sorted.bam"; exit 1; }
 
-echo "The duplicates of ${sample_name} have been marked."
-echo "The marked file is saved in $OUTPUT_PATH/${sample_name}_marked.bam."
+
