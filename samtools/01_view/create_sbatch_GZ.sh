57084e0af7602d011bf3aa932ad4cb043ad3c28c
Lucas04-nhr <lucas04@foxmail.com>
2024-08-05 11:47:57 +0800

    Refactor sbatch.list file names for BJ and GZ samples

--- samtools/01_view/create_sbatch_GZ.sh
+++ samtools/01_view/create_sbatch_GZ.sh
@@ -8,10 +8,10 @@ export GENOME_PATH=$genome_path
 echo "The path to the genome data has been set to $GENOME_PATH."
 
 # Check whether the index exists
-if [ ! -f "./gz_01_sbatch.list" ]
+if [ ! -f "./GZ_sbatch.list" ]
 then
     echo "The index does not exist, creating it ..."
-    touch ./gz_01_sbatch.list
+    touch ./GZ_sbatch.list
 else
     echo "The index already exists, do you want to overwrite it? (y/n)"
     read answer
@@ -30,7 +30,8 @@ fi
 echo "Adding the genome data to the config file..."
 for file_fq1 in $(ls ${GENOME_PATH})
 do
-    echo "$GENOME_PATH/$file_fq1" >> ./gz_01_sbatch.list
+    echo "$GENOME_PATH/$file_fq1" >> ./GZ_sbatch.list
+    sample_name=$(echo "$file_fq1" | grep -oE 'GZ[0-9]{3}')
     echo "The genome data of $sample_name has been added to the config file."
 done
 
