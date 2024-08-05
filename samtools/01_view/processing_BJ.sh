d65bad859ecff460dc37174e7d9b164ddb7bbd98
Lucas04-nhr <lucas04@foxmail.com>
2024-08-05 12:02:18 +0800

    Refactor samtools view command for better performance

--- samtools/01_view/processing_BJ.sh
+++ samtools/01_view/processing_BJ.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
 #SBATCH --export=IMPUT_PATH='/mnt/raid6/bacphagenetwork/data/01_bwa_analysis/Beijing',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/02_samtools_viewed/Beijing'
-#SBATCH --array=1-201%4
+#SBATCH --array=2-201%4
 
 echo "Initialization is complete."
 
@@ -19,6 +19,6 @@ echo "Processing $sample_name..."
 echo "Converting the SAM file to BAM file..."
 echo "The path to the SAM file is $IMPUT_PATH/${sample_name}.sam."
 echo "The path to the BAM file is $OUTPUT_PATH/${sample_name}.bam."
-samtools view -bHS $IMPUT_PATH/${sample_name}.sam > $OUTPUT_PATH/${sample_name}.bam || { echo "Error: samtools view failed in processing $sample_name."; exit 1; }
+samtools view -bS $IMPUT_PATH/${sample_name}.sam > $OUTPUT_PATH/${sample_name}.bam || { echo "Error: samtools view failed in processing $sample_name."; exit 1; }
 
 echo "The SAM file $sample_name has been successfully converted to BAM file."
