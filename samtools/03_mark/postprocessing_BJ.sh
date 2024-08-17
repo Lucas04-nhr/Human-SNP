c2ff930f3e2f34d10657719e8522c0a82102ab4a
Lucas04-nhr <lucas04@foxmail.com>
2024-08-17 11:18:47 +0800

    chore: Update PICARD_BIN path in check.sh script

--- samtools/03_mark/postprocessing_BJ.sh
+++ samtools/03_mark/postprocessing_BJ.sh
@@ -1,21 +1,8 @@
 #! /bin/bash
 
 # Initialize the environment
-echo "Initializing..."
 export INPUT_PATH='/mnt/raid6/bacphagenetwork/data/03_samtools_sorted/Beijing'
 export OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/04_dulplicate_marked/Beijing'
-
-# Check the metrics file
-echo "Checking the metrics file..."
-if [ ! -f "$OUTPUT_PATH/*.marked.metrics.txt" ]
-then
-    echo "Error: $OUTPUT_PATH/*.marked.metrics.txt does not exist."
-    exit 1
-else
-    echo "The file $OUTPUT_PATH/*.marked.metrics.txt exists."
-fi
-
-echo "Initializing complete."
 
 # Copy the metrics file
 echo "Copying the metrics file..."
@@ -28,6 +15,11 @@ then
     mkdir ./metrics/Beijing
 fi
 
-cp $OUTPUT_PATH/*.marked.metrics.txt ./metrics/Beijing || { echo "Error: Failed to copy the metrics file of $OUTPUT_PATH/*.marked.metrics.txt"; exit 1; }
+for file in $(ls $OUTPUT_PATH/*.marked.metrics.txt)
+do
+    echo "Copying $file..."
+    cp $file ./metrics/Beijing \
+    || { echo "Error: Failed to copy $file"; exit 1; }
+done
 
-echo "The metrics file has been copied."
+echo "The metrics files of Beijing have been copied."
