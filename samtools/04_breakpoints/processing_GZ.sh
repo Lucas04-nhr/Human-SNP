1ff37c2f307d33851e1a32460f4c9559814710b6
Lucas04-nhr <lucas04@foxmail.com>
2024-08-21 16:47:15 +0800

    chore: Update sbatch script to use OUTPUT instead of METRICS_FILE for insert size metrics

--- samtools/04_breakpoints/processing_GZ.sh
+++ samtools/04_breakpoints/processing_GZ.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=mark_breakpoints_BJ
-#SBATCH --output=./log/Beijing/mark_breakpoints_BJ_%j.out
-#SBATCH --error=./log/Beijing/mark_breakpoints_BJ_%j.err
+#SBATCH --job-name=mark_breakpoints_GZ
+#SBATCH --output=./log/Guangzhou/mark_breakpoints_GZ_%j.out
+#SBATCH --error=./log/Guangzhou/mark_breakpoints_GZ_%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
-#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/04_dulplicate_marked/Beijing',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/05_breakpoint_marked/Beijing',JAVA_BIN='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/bin/java',CPPFLAGS='-I/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/include',LDFLAGS='-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/:-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/server',GATK_OLD_BIN='/mnt/raid6/bacphagenetwork/tools/gatk-4.3.0.0/gatk-package-4.3.0.0-local.jar',GATK_NEW_BIN='/mnt/raid6/bacphagenetwork/tools/gatk-4.5.0.0/gatk-package-4.5.0.0-local.jar',PICARD_NEW_BIN='/mnt/raid6/bacphagenetwork/tools/picard_pre-built/v3.0/picard.jar',PICARD_OLD_BIN='/mnt/raid6/bacphagenetwork/tools/picard_pre-built/v2.26.0/picard.jar'
-#SBATCH --array=1
+#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/04_dulplicate_marked/Guangzhou',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/05_breakpoint_marked/Guangzhou',JAVA_BIN='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/bin/java',CPPFLAGS='-I/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/include',LDFLAGS='-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/:-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/server',GATK_OLD_BIN='/mnt/raid6/bacphagenetwork/tools/gatk-4.3.0.0/gatk-package-4.3.0.0-local.jar',GATK_NEW_BIN='/mnt/raid6/bacphagenetwork/tools/gatk-4.5.0.0/gatk-package-4.5.0.0-local.jar',PICARD_NEW_BIN='/mnt/raid6/bacphagenetwork/tools/picard_pre-built/v3.0/picard.jar',PICARD_OLD_BIN='/mnt/raid6/bacphagenetwork/tools/picard_pre-built/v2.26.0/picard.jar'
+#SBATCH --array=1-160%5
 
 # Initialize the environment
 echo "Initializing..."
@@ -13,8 +13,8 @@ echo "Initializing..."
 echo "The original *.bam files and their index are located in $INPUT_PATH."
 echo "The marked *.bam files will be saved in $OUTPUT_PATH."
 
-infile=($( cat bj_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
-sample_name=$(echo "$infile" | grep -oE 'BJ[0-9]{3}')
+infile=($( cat gz_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
+sample_name=$(echo "$infile" | grep -oE 'GZ[0-9]{3}')
 
 # Check all the needed files exist
 
