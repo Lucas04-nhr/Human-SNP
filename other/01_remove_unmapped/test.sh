9db8e516c48b2af990263d81f76d3c35bcab57cc
Lucas04-nhr <lucas04@foxmail.com>
2024-08-25 17:32:34 +0800

    chore: Update processing scripts to remove unmapped lines from SAM files

--- other/01_remove_unmapped/test.sh
+++ other/01_remove_unmapped/test.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-# Remove duplicates
-echo "Removing duplicates from $INPUT_PATH/${sample_name}.marked.sam to $OUTPUT_PATH/${sample_name}.removed.sam..."
+# Remove unmapped
+
+# Calculate the number of lines
+export total_lines=$(wc -l < ./BJ001.head50.sam | grep -oE '[0-9]+')
+echo "The total number of lines is $total_lines."
+echo "========================================================================"
+echo "Processing..."
+
+
+export i=0
 
 while read -r line; do
-
+    export i=$((i+1))
+    echo "Processing line $i..."
     if [[ ! $line =~ \*[\ ]*0[\ ]*0[\ ]*\*[\ ]*\*[\ ]*0[\ ]*0 ]]; then
         echo "$line" >> ./BJ001.head50.removed.sam
+        echo "Line $i will be saved."
+    else
+        echo "Line $i will be removed."
     fi
 done < ./BJ001.head50.sam
+
+echo "========================================================================"
+echo "Removing unmapped complete."
