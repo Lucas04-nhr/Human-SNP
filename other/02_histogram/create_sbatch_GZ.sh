8a48cbafc1a45f0e4e31487f67c19540c1691e99
Lucas04-nhr <lucas04@foxmail.com>
2024-08-27 16:45:50 +0800

    Refactor processing scripts to use original *.sam data instead of marked *.bam data for Beijing and Guangzhou datasets

--- other/02_histogram/create_sbatch_GZ.sh
+++ other/02_histogram/create_sbatch_GZ.sh
@@ -20,15 +20,15 @@ else
 fi
 
 # Set the path to the genome data
-echo "Please enter the path to the marked *.bam data of Guangzhou:"
+echo "Please enter the path to the original *.sam data of Guangzhou:"
 read genome_path
-# The path of the marked *.bam data is '/mnt/raid6/bacphagenetwork/data/06_unmapped_removed/Guangzhou'
+# The path of the original *.sam data is '/mnt/raid6/bacphagenetwork/data/06_unmapped_removed/Guangzhou'
 export GENOME_PATH=$genome_path
 echo "The path to the genome data has been set to $GENOME_PATH."
 
 # Add the genome data to the config file "samples.index"
 echo "Adding the genome data to the config file..."
-for file_fq1 in $(ls ${GENOME_PATH} | grep -E '.marked.sam')
+for file_fq1 in $(ls ${GENOME_PATH} | grep -E '.removed.sam')
 do
     echo "$GENOME_PATH/$file_fq1" >> ./GZ_sbatch.list
     sample_name=$(echo "$file_fq1" | grep -oE 'GZ[0-9]{3}')
