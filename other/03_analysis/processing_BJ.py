90e2042b1a0793795d74eee837970e696d900733
Lucas04-nhr <lucas04@foxmail.com>
2024-09-02 16:39:34 +0800

    chore: Refactor processing scripts to use original *.sam data instead of marked *.bam data for Beijing and Guangzhou datasets

--- other/03_analysis/processing_BJ.py
+++ other/03_analysis/processing_BJ.py
@@ -33,11 +33,16 @@ def save_counts_to_static_file(counts_list, static_file):
         for count in counts_list:
             outfile.write(f"{count}\n")
 
+def draw_pie_chart(counts_list, output_file):
+    plt.figure(figsize=(10, 10), dpi=300)
+    plt.pie(counts_list, autopct='%1.1f%%')
+    plt.savefig(output_file)
+
 
 # Set up the argument parser
 parser = argparse.ArgumentParser(description="Process a SAM file.")
 parser.add_argument('-i', '--input', required=True, help="Input SAM file")
-parser.add_argument('-s', '--static', required=False, help="Static file")
+parser.add_argument('-s', '--static', required=False, help="Static file directory")
 parser.add_argument('-o', '--output', required=True, help="Output directory")
 
 args = parser.parse_args()
@@ -47,7 +52,8 @@ input_file = args.input
 sample_name = extract_sample_name(input_file)
 output_dir = args.output
 tmp_dir = os.path.join(output_dir, "tmp")
-output_file = os.path.join(output_dir, os.path.basename(input_file) + ".processed.txt")
+static_dir = args.static
+output_file = os.path.join(output_dir, os.path.basename(input_file) + ".piechart.pdf")
 tmp_file = os.path.join(tmp_dir, os.path.basename(input_file) + ".tmp")
 
 # Check if sample name was extracted
@@ -78,7 +84,8 @@ counts_list = count_elements_in_tmp_file(tmp_file)
 
 # If -s is provided, save the counts to the static file using *.csv format
 if args.static:
-    save_counts_to_static_file(counts_list, args.static)
+    static_file = os.path.join(static_dir, sample_name + ".static.csv")
+    save_counts_to_static_file(counts_list, static_file)
     
 # Draw the pie chart
 
