add9697c03a6098748243475d35c407fece9c1c2
Lucas04-nhr <lucas04@foxmail.com>
2024-09-02 16:26:05 +0800

    chore: Refactor processing scripts to use original *.sam data instead of marked *.bam data for Beijing and Guangzhou datasets

---
+++ other/03_analysis/processing_BJ.py
@@ -0,0 +1,73 @@
+import pysam
+import argparse
+from collections import Counter
+import os
+import re
+
+def extract_sample_name(input_file):
+    match = re.search(r'BJ[0-9]{3}', input_file)
+    if match:
+        sample_name = match.group(0)
+    else:
+        sample_name = "NoMatchFound"
+    return sample_name
+
+def count_elements_in_tmp_file(tmp_file):
+    element_counts = {}
+
+    with open(tmp_file, "r") as infile:
+        for line in infile:
+            element = line.strip()
+            if element in element_counts:
+                element_counts[element] += 1
+            else:
+                element_counts[element] = 1
+
+    counts_list = list(element_counts.values())
+    return counts_list
+
+
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Process a SAM file.")
+parser.add_argument('-i', '--input', required=True, help="Input SAM file")
+parser.add_argument('-s', '--static', required=False, help="Static file")
+parser.add_argument('-o', '--output', required=True, help="Output directory")
+
+args = parser.parse_args()
+
+# Get the input and output file names
+input_file = args.input
+sample_name = extract_sample_name(input_file)
+output_dir = args.output
+tmp_dir = os.path.join(output_dir, "tmp")
+output_file = os.path.join(output_dir, os.path.basename(input_file) + ".processed.txt")
+tmp_file = os.path.join(tmp_dir, os.path.basename(input_file) + ".tmp")
+
+# Check if sample name was extracted
+if sample_name == "NoMatchFound":
+    print("No sample name found in the input file name. Please check the input file name.")
+    exit(127)
+
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+
+# Open the SAM file
+print("Processing file: " + input_file)
+samfile = pysam.AlignmentFile(input_file, "r")
+
+# Open the temporary file and write the header
+with open(tmp_file, "w") as outfile:
+  for read in samfile.fetch():
+        outfile.write(f"{read.flag}\n")
+
+# Close the SAM file
+samfile.close()
+
+# Open the temporary file and count the flags
+counts_list = count_elements_in_tmp_file(tmp_file)
+
+# If -s is provided, save the counts to the static file
+
