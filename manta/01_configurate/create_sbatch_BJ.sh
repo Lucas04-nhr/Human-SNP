472b08d32320e628621465b3561e45978a54a992
Lucas04-nhr <lucas04@foxmail.com>
2024-09-08 21:27:04 +0800

    Refactor file paths and filter out .bam.bai files in create_sbatch_BJ.sh and create_sbatch_GZ.sh

--- manta/01_configurate/create_sbatch_BJ.sh
+++ manta/01_configurate/create_sbatch_BJ.sh
@@ -28,7 +28,7 @@ echo "The path to the genome data has been set to $GENOME_PATH."
 
 # Add the genome data to the config file "samples.index"
 echo "Adding the genome data to the config file..."
-for file_fq1 in $(ls ${GENOME_PATH} | grep -E '.bam')
+for file_fq1 in $(ls ${GENOME_PATH} | | grep -E '\.bam$' | grep -vE '\.bam\.bai$')
 do
     echo "$GENOME_PATH/$file_fq1" >> ./BJ_sbatch.list
     sample_name=$(echo "$file_fq1" | grep -oE 'BJ[0-9]{3}')
