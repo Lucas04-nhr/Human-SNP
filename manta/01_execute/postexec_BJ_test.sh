7fed366bc4c2a17d046eca5ae494b66d78c347b8
Lucas04-nhr <lucas04@foxmail.com>
2024-09-08 23:10:02 +0800

    Refactor Manta analysis script to add script for executing analysis on Beijing dataset

---
+++ manta/01_execute/postexec_BJ_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=manta_copy_result_BJ
+#SBATCH --output=./log/Beijing/manta_copy_result_BJ_%j.out
+#SBATCH --error=./log/Beijing/manta_copy_result_BJ_%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=1G
+#SBATCH --export=PARENT_PATH='/mnt/raid6/bacphagenetwork/data/07_manta/01_exec/Beijing',MANTA_INSTALL_PATH='/mnt/raid6/bacphagenetwork/tools/manta',RESULT_PATH='/mnt/raid6/bacphagenetwork/niehaoran/Human-SNP/manta/02_result/Beijing'
+#SBATCH --array=1
+
+# Initialize the environment
+echo "Initializing..."
+
+echo "The parent folder of the execute folders is located in $PARENT_PATH."
+
+infile=($( cat BJ_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
+sample_name=$(echo "$infile" | grep -oE 'BJ[0-9]{3}')
+
+export MANTA_ANALYSIS_PATH=$PARENT_PATH/$sample_name
+echo "The path to the execute folder of $sample_name has been set to $MANTA_ANALYSIS_PATH."
+
+echo "Initializing complete."
+echo "========================================"
+
+# Copy the Manta analysis result
+echo "Copying the Manta analysis result of ${sample_name}..."
+export STATS_PATH=${MANTA_ANALYSIS_PATH}/results/stats
+
+if [ ! -d "${RESULT_PATH}/${sample_name}" ]
+then
+    mkdir -p ${RESULT_PATH}/${sample_name}
+    echo "The result folder has been created."
+else
+    echo "The result folder already exists,overwriting it..."
+    rm -rf ${RESULT_PATH}/${sample_name}
+fi
+
+cp -r ${STATS_PATH} ${RESULT_PATH}/${sample_name} \
+|| { echo "Error: Copying the Manta analysis result failed."; exit 1; }
+
+echo "========================================"
+echo "The Manta analysis result has been copied successfully."
