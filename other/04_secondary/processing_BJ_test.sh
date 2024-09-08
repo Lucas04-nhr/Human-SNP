a71347b2aa14c7d8832b3e42a16f42326ff87d50
Lucas04-nhr <lucas04@foxmail.com>
2024-09-08 16:58:06 +0800

    Refactor processing script to add conversion of sam file to bam file and export config file

--- other/04_secondary/processing_BJ_test.sh
+++ other/04_secondary/processing_BJ_test.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=./log/Beijing/secondary_BJ_%j.err
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=1G
-#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/06_unmapped_removed/Beijing',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/09_secondary/Beijing',BREAKDANCER_BIN='/mnt/raid6/bacphagenetwork/tools/breakdancer/build/bin/breakdancer-max'
+#SBATCH --export=INPUT_PATH='/mnt/raid6/bacphagenetwork/data/06_unmapped_removed/Beijing',OUTPUT_PATH='/mnt/raid6/bacphagenetwork/data/09_secondary/Beijing',OMP_NUM_THREADS='2',DELLY_BIN
 #SBATCH --array=1
 
 # Initialate conda
