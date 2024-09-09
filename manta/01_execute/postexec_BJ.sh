8dc5edc1006efa27bd452bad2516b63d4dac9c11
Lucas04-nhr <lucas04@foxmail.com>
2024-09-09 09:15:45 +0800

    Refactor Manta analysis script to update job dependency

--- manta/01_execute/postexec_BJ.sh
+++ manta/01_execute/postexec_BJ.sh
@@ -6,7 +6,6 @@
 #SBATCH --mem=1G
 #SBATCH --export=PARENT_PATH='/mnt/raid6/bacphagenetwork/data/07_manta/01_exec/Beijing',MANTA_INSTALL_PATH='/mnt/raid6/bacphagenetwork/tools/manta',RESULT_PATH='/mnt/raid6/bacphagenetwork/niehaoran/Human-SNP/manta/02_result/Beijing'
 #SBATCH --array=2-201%5
-#SBATCH --dependency=afterok:59100
 
 # Initialize the environment
 echo "Initializing..."
