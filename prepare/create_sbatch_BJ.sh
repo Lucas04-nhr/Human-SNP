0c6ff15c440888f01ada56ffe718c296f7d0942d
Lucas04-nhr <lucas04@foxmail.com>
2024-09-18 11:10:32 +0800

    Refactor create_sbatch_BJ.sh and create_sbatch_GZ.sh scripts to improve genome data path handling

--- prepare/create_sbatch_BJ.sh
+++ prepare/create_sbatch_BJ.sh
@@ -1,37 +1,38 @@
 #! /bin/bash
 
+# Set the path to the genome data
+echo "Please enter the path to the genome data:"
+read genome_path
+# The path of the genome data is '/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host'
+export GENOME_PATH=$genome_path
+echo "The path to the genome data has been set to $GENOME_PATH."
+
 # Check whether the index exists
-if [ ! -f "./bj_sbatch.list" ]
+if [ ! -f "./BJ_sbatch.list" ]
 then
     echo "The index does not exist, creating it ..."
-    touch ./bj_sbatch.list
+
 else
     echo "The index already exists, do you want to overwrite it? (y/n)"
     read answer
     if [ $answer == "y" ] || [ $answer == "Y" ]
     then
         echo "Overwriting the index ..."
-        rm ./bj_sbatch.list
-        touch ./bj_sbatch.list
     else
         echo "The index will not be overwritten."
         exit
     fi
 fi
 
-# Set the path to the genome data
-echo "Please enter the path to the marked *.bam data of Beijing:"
-read genome_path
-# The path of the marked *.bam data is '/mnt/raid6/bacphagenetwork/data/04_dulplicate_marked/Beijing'
-export GENOME_PATH=$genome_path
-echo "The path to the genome data has been set to $GENOME_PATH."
-
 # Add the genome data to the config file "samples.index"
 echo "Adding the genome data to the config file..."
-for file_fq1 in $(ls ${GENOME_PATH} | grep -E '.marked.bam')
+for file_fq1 in $(ls ${GENOME_PATH}/*_1.fastq.gz)
 do
-    echo "$GENOME_PATH/$file_fq1" >> ./bj_sbatch.list
-    sample_name=$(echo "$file_fq1" | grep -oE 'BJ[0-9]{3}')
+    file_name=$(basename "$file_fq1")
+    sample_name="${file_name%%_1*}"
+    echo "Processing $sample_name..."
+    file_fq2="${sample_name}_2.fastq.gz"
+    echo "$file_fq1" >> ./slurm/bj_sbatch.list
     echo "The genome data of $sample_name has been added to the config file."
 done
 
