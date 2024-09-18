663e9ffe2c5b14f08ddfdf638486d775fd9a4e0d
Lucas04-nhr <lucas04@foxmail.com>
2024-09-18 11:09:11 +0800

    Refactor check.sh script to improve folder creation and log handling

---
+++ prepare/create_sbatch_BJ.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# Check whether the index exists
+if [ ! -f "./bj_sbatch.list" ]
+then
+    echo "The index does not exist, creating it ..."
+    touch ./bj_sbatch.list
+else
+    echo "The index already exists, do you want to overwrite it? (y/n)"
+    read answer
+    if [ $answer == "y" ] || [ $answer == "Y" ]
+    then
+        echo "Overwriting the index ..."
+        rm ./bj_sbatch.list
+        touch ./bj_sbatch.list
+    else
+        echo "The index will not be overwritten."
+        exit
+    fi
+fi
+
+# Set the path to the genome data
+echo "Please enter the path to the marked *.bam data of Beijing:"
+read genome_path
+# The path of the marked *.bam data is '/mnt/raid6/bacphagenetwork/data/04_dulplicate_marked/Beijing'
+export GENOME_PATH=$genome_path
+echo "The path to the genome data has been set to $GENOME_PATH."
+
+# Add the genome data to the config file "samples.index"
+echo "Adding the genome data to the config file..."
+for file_fq1 in $(ls ${GENOME_PATH} | grep -E '.marked.bam')
+do
+    echo "$GENOME_PATH/$file_fq1" >> ./bj_sbatch.list
+    sample_name=$(echo "$file_fq1" | grep -oE 'BJ[0-9]{3}')
+    echo "The genome data of $sample_name has been added to the config file."
+done
+
+# Check if any error occurred during the process
+if [ $? -eq 0 ]; then
+    echo "Success!"
+else
+    echo "Warning: An error occurred during the process."
+    exit 1
+fi
