d4dace3ea06b3a6fc63ab2b5ae3743b667ffa992
Lucas04-nhr <lucas04@foxmail.com>
2024-09-20 11:40:28 +0800

    Refactor folder creation in check.sh script and add tag creation in processing scripts

---
+++ prepare/processing02_BJ.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --job-name=preprocessing_BJ
+#SBATCH --output=./log/Beijing/preprocessing_BJ.%j.out
+#SBATCH --error=./log/Beijing/preprocessing_BJ.%j.err
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=1G
+#SBATCH --export=DATA_PATH='/mnt/raid6/bacphagenetwork/data/skin_metagenome/Beijing/02_rm_host',INDEXING_PATH='/mnt/raid6/bacphagenetwork/data/00_bwa_index/chm13v2/chm13v2.0_noY.fa',OUTPUT_BASE_PATH='/mnt/raid6/bacphagenetwork/data/'
+#SBATCH --array=1-201%4
+
+# Initialize the environment
+echo "Initializing..."
+
+# Set the paths of the output files
+ALIGNED_DATA_PATH="$OUTPUT_BASE_PATH/01_align/Beijing"
+FILTERED_DATA_PATH="$OUTPUT_BASE_PATH/02_filter/Beijing"
+SORTED_DATA_PATH="$OUTPUT_BASE_PATH/03_sort/Beijing"
+CONVERTED_DATA_PATH="$OUTPUT_BASE_PATH/04_convert/Beijing"
+ADDED_TAG_DATA_PATH="$OUTPUT_BASE_PATH/03_tag_added/Beijing"
+
+echo "The original *.fastq files are located in $DATA_PATH."
+echo "The indexing data is located in $INDEXING_PATH."
+echo "The alignment results will be saved in $ALIGNED_DATA_PATH."
+echo "The *.bam files removed unmapped reads will be saved in $FILTERED_DATA_PATH."
+echo "The sorted *.bam files and their indexes will be saved in $SORTED_DATA_PATH."
+echo "The converted *.sam files will be saved in $CONVERTED_DATA_PATH."
+
+echo "Initializing completed."
+echo "=============================="
+
+# Get the list of all *.fastq files
+infile=($( cat BJ_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
+file_base=$(basename "$infile" _1.fastq.gz)
+
+# Extract the sample name
+if [[ $infile == *Beijing* ]]; then
+  sample_name=$(echo "$infile" | grep -oE 'BJ[0-9]{3}')
+elif [[ $infile == *Guangzhou* ]]; then
+  sample_name_temp=$(echo "$infile" | awk -F'/' '{n=split($NF,a,"_"); printf "%03d", a[n-1]}')
+  sample_name="GZ$sample_name_temp"
+else
+  echo "Error: The sample name cannot be extracted."
+  exit 1
+fi
+
+echo "Processing $sample_name..."
+
+# Set the paths of the input files
+GENE_DATA_1="$DATA_PATH/${file_base}_1.fastq.gz"
+GENE_DATA_2="$DATA_PATH/${file_base}_2.fastq.gz"
+
+# Step 5: Add the tag to the reads
+echo "Adding the tag to the reads..."
+samtools addreplacerg -r 'ID:${sample_name}' -r 'SM=${sample_name}' -r 'PL:illumina' -r 'CN:BJ' $CONVERTED_DATA_PATH/${sample_name}.sam > $ADDED_TAG_DATA_PATH/${sample_name}.bam \
+|| { echo "Error: samtools addreplacerg failed in processing $sample_name."; exit 1; }
+echo "Indexing completed."
+
+
+# All done
+echo "=============================="
+echo "Processing $sample_name completed."
