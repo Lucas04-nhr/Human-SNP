3cf424326425499b620b1609da12544f38545022
Lucas04-nhr <lucas04@foxmail.com>
2024-09-26 22:48:40 +0800

    Update cpu cores and memory usage in processing script

--- gatk/processing_BJ.sh
+++ gatk/processing_BJ.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=processing_BJ
 #SBATCH --output=./log/Beijing/processing_BJ.%j.out
 #SBATCH --error=./log/Beijing/processing_BJ.%j.err
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=1G
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=2G
 #SBATCH --export=BASE_PATH='/mnt/raid6/bacphagenetwork/data/',GATK_OLD_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.3.0.0/gatk",GATK_NEW_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.5.0.0/gatk",
 #SBATCH --array=1-201%4
 
@@ -40,5 +40,41 @@ sample_name=$(basename "$infile" .bam)
 echo "Performing BaseRecalibrator for ${sample_name}..."
 $GATK_NEW_BIN BaseRecalibrator \
   -I $SORTED_DATA_PATH/${sample_name}.bam \
-  -R $INDEXING_PATH \
+  -R $INDEXING_FILE \
   --known-sites $KNOWN_SITES_FILE \
+  -O $RECALIBRATED_DATA_PATH/${sample_name}.recal_data.table \
+  --use-original-qualities \
+  --num-threads 5 \
+|| { echo "BaseRecalibrator for ${sample_name} failed"; exit 1; }
+
+echo "BaseRecalibrator for ${sample_name} completed."
+echo "=============================="
+
+# Perform the ApplyBQSR
+
+echo "Performing ApplyBQSR for ${sample_name}..."
+$GATK_NEW_BIN ApplyBQSR \
+  -I $SORTED_DATA_PATH/${sample_name}.bam \
+  -R $INDEXING_PATH \
+  --bqsr-recal-file $RECALIBRATED_DATA_PATH/${sample_name}.recal_data.table \
+  -O $APPLYBQSR_DATA_PATH/${sample_name}.recalibrated.bam \
+  --num-threads 5 \
+|| { echo "ApplyBQSR for ${sample_name} failed"; exit 1; }
+
+echo "ApplyBQSR for ${sample_name} completed."
+echo "=============================="
+
+# Perform the HaplotypeCaller
+
+echo "Performing HaplotypeCaller for ${sample_name}..."
+$GATK_NEW_BIN HaplotypeCaller \
+  -I $APPLYBQSR_DATA_PATH/${sample_name}.recalibrated.bam \
+  -R $INDEXING_PATH \
+  -O $HAPLOTYPECALLER_DATA_PATH/${sample_name}.g.vcf.gz \
+  --native-pair-hmm-threads 5 \
+|| { echo "HaplotypeCaller for ${sample_name} failed"; exit 1; }
+
+echo "HaplotypeCaller for ${sample_name} completed."
+echo "=============================="
+
+echo "All processes completed."
