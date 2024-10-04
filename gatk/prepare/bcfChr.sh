06e89755e39522b155e8a21ea25558bab577d3ad
Lucas04-nhr <lucas04@foxmail.com>
2024-10-04 21:59:59 +0800

    Refactor script to modify chromosome names in VCF file using bcftools

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -3,9 +3,9 @@
 export PATH=/mnt/raid6/bacphagenetwork/tools/bcftools/bin/:$PATH
 
 # Define the input and output file paths
-export INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
-export OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
-export OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.idx"
+INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
+OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
+OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.idx"
 
 # Modify chromosome names in the VCF file
 bcftools annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
