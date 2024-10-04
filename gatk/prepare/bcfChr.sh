9dab6a5c16660f989843fa056c3f7828e745bdf4
Lucas04-nhr <lucas04@foxmail.com>
2024-10-04 21:59:42 +0800

    Refactor script to use bcftools binary path variable and update PATH

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export BCFTOOLS_BIN=“/mnt/raid6/bacphagenetwork/tools/bcftools/bin/bcftools”
+export PATH=/mnt/raid6/bacphagenetwork/tools/bcftools/bin/:$PATH
 
 # Define the input and output file paths
 export INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
@@ -8,10 +8,10 @@ export OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sit
 export OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.idx"
 
 # Modify chromosome names in the VCF file
-$BCFTOOLS_BIN annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
+bcftools annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
 
 # Reindex new VCF file
-$BCFTOOLS_BIN index $OUTPUT_VCF -f $OUTPUT_INDEX
+bcftools index $OUTPUT_VCF -f $OUTPUT_INDEX
 
 echo "Chromosome names have been modified and saved to $OUTPUT_VCF."
 echo "Output file has already been indexed and saved to $OUTPUT_INDEX."
