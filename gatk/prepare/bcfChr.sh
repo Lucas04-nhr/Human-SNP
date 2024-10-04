16fc28aebeea3935d8591610524ba6dc9ed6fc28
Lucas04-nhr <lucas04@foxmail.com>
2024-10-04 21:55:43 +0800

    Refactor script to use bcftools binary path variable and add indexing step

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
-#SBATCH --job-name=modify_vcf
-#SBATCH --output=modify_vcf_%j.log
-#SBATCH --error=modify_vcf_%j.err
-#SBATCH --ntasks=1
-#SBATCH --time=01:00:00
-#SBATCH --mem=4G
-
-# Load necessary modules
-module load bcftools
+export BCFTOOLS_BIN=“/mnt/raid6/bacphagenetwork/tools/bcftools/bin/bcftools”
 
 # Define the input and output file paths
-INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
-OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
+export INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
+export OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
+export OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.idx"
 
 # Modify chromosome names in the VCF file
-bcftools annotate --rename-chrs chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
+$BCFTOOLS_BIN annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
 
-echo "Chromosome names have been modified and saved to $OUTPUT_VCF"
+# Reindex new VCF file
+$BCFTOOLS_BIN index $OUTPUT_VCF -f $OUTPUT_INDEX
+
+echo "Chromosome names have been modified and saved to $OUTPUT_VCF."
+echo "Output file has already been indexed and saved to $OUTPUT_INDEX."
