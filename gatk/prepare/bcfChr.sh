3af2d28715d37be572f85616bda6f8fe5fbb40d5
Lucas04-nhr <lucas04@foxmail.com>
2024-10-04 22:34:14 +0800

    Refactor script to modify chromosome names in VCF file using bcftools

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -5,13 +5,13 @@ export PATH=/mnt/raid6/bacphagenetwork/tools/bcftools/bin/:$PATH
 # Define the input and output file paths
 INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
 OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
-OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.idx"
+OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf.idx"
 
 # Modify chromosome names in the VCF file
 bcftools annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
 
 # Reindex new VCF file
-bcftools index $OUTPUT_VCF -f $OUTPUT_INDEX
+gatk IndexFeatureFile -i $OUTPUT_VCF
 
 echo "Chromosome names have been modified and saved to $OUTPUT_VCF."
 echo "Output file has already been indexed and saved to $OUTPUT_INDEX."
