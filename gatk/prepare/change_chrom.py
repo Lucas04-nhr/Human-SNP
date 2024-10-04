97dae3b4b5dbb71c0fbc2ca4c9021af894cf61f1
Lucas04-nhr <lucas04@foxmail.com>
2024-10-04 20:12:19 +0800

    Refactor script to modify 'chr' prefix in CHROM field of VCF file using pysam

--- gatk/prepare/change_chrom.py
+++ gatk/prepare/change_chrom.py
@@ -2,31 +2,21 @@ import os
 import pysam
 import argparse
 
-def remove_chr_prefix(input_vcf, output_vcf):
-    # Open the input VCF file with pysam
-    vcf_in = pysam.VariantFile(input_vcf, 'r')
-    # Open the output VCF file with pysam
-    vcf_out = pysam.VariantFile(output_vcf, 'w', header=vcf_in.header)
+def modify_chrom_prefix(input_vcf, output_vcf):
+    with pysam.VariantFile(input_vcf) as vcf_in, pysam.VariantFile(output_vcf, 'w', header=vcf_in.header) as vcf_out:
+        for record in vcf_in:
+            print("CHROM field before modification:", record.chrom)
+            # Modify the CHROM field, remove the 'chr' prefix
+            if record.chrom.startswith('chr'):
+                print("Removing 'chr' prefix from CHROM field")
+                record.chrom = record.chrom[3:]  # Remove the 'chr' prefix
+                print("CHROM field after modification:", record.chrom)
+            else:
+                print("CHROM field does not start with 'chr'")
+            
+            # Write the modified record to the output VCF file
+            vcf_out.write(record)
 
-    for record in vcf_in:
-        print("CHROM field before modification:", record.chrom)
-        # Modify the CHROM field, remove the 'chr' prefix
-        if record.chrom.startswith('chr'):
-            print("Removing 'chr' prefix from CHROM field")
-            new_chrom = record.chrom[3:]  # Remove the 'chr' prefix
-            print("CHROM field after modification:", new_chrom)
-            record.chrom = new_chrom
-            if new_chrom not in vcf_out.header.contigs:
-                vcf_out.header.contigs.add(new_chrom)
-        else:
-            print("CHROM field does not start with 'chr'")
-        
-        # Write the record to the output VCF file
-        vcf_out.write(record)
-
-    # Close the VCF files
-    vcf_in.close()
-    vcf_out.close()
 
 # Parse the input arguments
 parser = argparse.ArgumentParser(description='Remove the chr prefix from the CHROM field in a VCF file')
