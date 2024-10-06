94b56ac79350788af9363b4b33341b0343e6efff
Lucas04-nhr <lucas04@foxmail.com>
2024-10-06 17:03:50 +0800

    Refactor script to update path of known sites VCF file

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -4,8 +4,8 @@ export PATH=/mnt/raid6/bacphagenetwork/tools/bcftools/bin/:$PATH
 
 # Define the input and output file paths
 INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
-OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
-OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38_modified.dbsnp138.vcf.idx"
+OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/modified/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
+OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/modified/Homo_sapiens_assembly38_modified.dbsnp138.vcf.idx"
 
 # Modify chromosome names in the VCF file
 bcftools annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
