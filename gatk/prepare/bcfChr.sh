88fdfec30910dbb18905dc0498b231138b8d818a
Lucas04-nhr <lucas04@foxmail.com>
2024-10-11 14:17:52 +0800

    Refactor prepare script for WEScall pipeline

--- gatk/prepare/bcfChr.sh
+++ gatk/prepare/bcfChr.sh
@@ -2,20 +2,6 @@
 
 export PATH=/mnt/raid6/bacphagenetwork/tools/bcftools/bin/:$PATH
 
-# Define the input and output file paths
-INPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/Homo_sapiens_assembly38.dbsnp138.vcf"
-OUTPUT_VCF="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/modified/Homo_sapiens_assembly38_modified.dbsnp138.vcf"
-OUTPUT_INDEX="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/modified/Homo_sapiens_assembly38_modified.dbsnp138.vcf.idx"
-
-# Modify chromosome names in the VCF file
-bcftools annotate --rename-chrs ./chr_names.txt $INPUT_VCF -o $OUTPUT_VCF
-
-# Reindex new VCF file
-gatk IndexFeatureFile -I $OUTPUT_VCF
-
-echo "Chromosome names have been modified and saved to $OUTPUT_VCF."
-echo "Output file has already been indexed and saved to $OUTPUT_INDEX."
-
 BASE_PATH="/mnt/raid6/bacphagenetwork/data/00_bwa_index/GRCh38/known-sites/"
 
 # Define arrays of input and output file paths
