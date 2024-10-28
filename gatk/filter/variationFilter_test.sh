70e6620b8aafe30bc83b808c96b463868a83b3d8
Lucas04-nhr <lucas04@foxmail.com>
2024-10-28 15:01:46 +0800

    Refactor variationFilter.sh script to remove unnecessary SLURM directives and update file paths

--- gatk/filter/variationFilter_test.sh
+++ gatk/filter/variationFilter_test.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=processing_BJ
-#SBATCH --output=./log/Beijing/processing_BJ.%j.out
-#SBATCH --error=./log/Beijing/processing_BJ.%j.err
+#SBATCH --job-name=test_filter
+#SBATCH --output=./test_log.%j.out
+#SBATCH --error=./test_log.%j.err
 #SBATCH --cpus-per-task=5
-#SBATCH --mem=2G
+#SBATCH --mem=16G
 #SBATCH --export=BASE_PATH='/mnt/raid6/bacphagenetwork/data/',GATK_OLD_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.3.0.0/gatk",GATK_NEW_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.5.0.0/gatk",JAVA_HOME='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/',JAVA_BIN='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/bin/java',LDFLAGS='-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/server',CPPFLAGS='-I/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/include'
-#SBATCH --array=1-201%4
 
 # Initialize the environment
 echo "Initializing..."
 
-
 # Define input and output files
 INPUT_VCF="$BASE_PATH/test/08_GenotypeGVCF/joint_genotyped.vcf.gz"
-OUTPUT_VCF="$BASE_PATH/test/08_GenotypeGVCF/filtered_output.vcf"
+OUTPUT_VCF="$BASE_PATH/test/08_GenotypeGVCF/filtered_output.vcf.gz"
 REFERENCE_GENOME="$BASE_PATH/00_bwa_index/GRCh38/Homo_sapiens.GRCh38.dna.toplevel.fa"
 
 # Define filter expressions
@@ -21,7 +19,7 @@ SNP_FILTER="QD < 2.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0"
 
 
 # Apply hard filters to SNPs
-gatk VariantFiltration \
+$GATK_OLD_BIN VariantFiltration \
     -R $REFERENCE_GENOME \
     -V $INPUT_VCF \
     --filter-expression "$SNP_FILTER" \
