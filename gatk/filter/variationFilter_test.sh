3c0a0ba1a5d43758c1929f71e2c2d3af3a05eaee
Lucas04-nhr <lucas04@foxmail.com>
2024-10-28 15:01:53 +0800

    Refactor variationFilter_test.sh script to update memory allocation to 32G

--- gatk/filter/variationFilter_test.sh
+++ gatk/filter/variationFilter_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=./test_log.%j.out
 #SBATCH --error=./test_log.%j.err
 #SBATCH --cpus-per-task=5
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --export=BASE_PATH='/mnt/raid6/bacphagenetwork/data/',GATK_OLD_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.3.0.0/gatk",GATK_NEW_BIN="/mnt/raid6/bacphagenetwork/tools/gatk-4.5.0.0/gatk",JAVA_HOME='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/',JAVA_BIN='/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/bin/java',LDFLAGS='-L/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/lib/server',CPPFLAGS='-I/mnt/raid6/bacphagenetwork/tools/jdk-22.0.1/include'
 
 # Initialize the environment
