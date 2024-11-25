1efeb621cd04f18b47b795fa483854dd3ae38d6d
Lucas04-nhr <lucas04@foxmail.com>
2024-11-25 17:11:46 +0800

    Fix phenotype column extraction in add_bacteria_BJ.sh and add_bacteria_GZ.sh to use the last occurrence

--- concat/add_bacteria_GZ.sh
+++ concat/add_bacteria_GZ.sh
@@ -21,7 +21,7 @@ echo "=============================="
 infile=($( cat GZ_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
 
 # Extract the Phenotype Col Number
-pheno_col=$(echo $infile | grep -o -E '[0-9]+' | head -n 2)
+pheno_col=$(echo $infile | grep -o -E '[0-9]+' | tail -n 1)
 echo "The phenotype column number is $pheno_col."
 python3 ./add_bacteria.py --input-file=${infile} \
 || { echo "Error in add_bacteria.py"; exit 1; }
