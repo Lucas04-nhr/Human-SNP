318c3dc80b8f8850d143ee6dbcff643b625fd00a
Lucas04-nhr <lucas04@foxmail.com>
2024-11-27 18:58:26 +0800

    Add new plink scripts for Beijing and Guangzhou analyses with environment setup and processing steps

--- plink/plink_full_numeric_GZ.sh
+++ plink/plink_full_numeric_GZ.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-#SBATCH --job-name=plink_BJ
-#SBATCH --output=./BJ_log.%j.out
-#SBATCH --error=./BJ_log.%j.err
+#SBATCH --job-name=plink_GZ
+#SBATCH --output=./GZ_log.%j.out
+#SBATCH --error=./GZ_log.%j.err
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
@@ -42,9 +42,9 @@ export KNOWN_SITES_DBSNP="$KNOWN_SITES_BASE_PATH/dbsnp138/hg38_v0_Homo_sapiens_a
 export KNOWN_SITES_HAPMAP="$KNOWN_SITES_BASE_PATH/hapmap/hg38_v0_hapmap_3.3.hg38.modified.vcf"
 export KNOWN_SITES_OMNI="$KNOWN_SITES_BASE_PATH/omni/hg38_v0_1000G_omni2.5.hg38.modified.vcf"
 
-export UNFILTERED_GVCF_PATH="$BASE_PATH/08_GenotypeGVCF/Beijing"
-export FILTERED_GVCF_PATH="$BASE_PATH/10_ApplyVQSR/Beijing"
-export PLINK_PATH="$BASE_PATH/12_plink/Beijing"
+export UNFILTERED_GVCF_PATH="$BASE_PATH/08_GenotypeGVCF/Guangzhou"
+export FILTERED_GVCF_PATH="$BASE_PATH/10_ApplyVQSR/Guangzhou"
+export PLINK_PATH="$BASE_PATH/12_plink/Guangzhou"
 export PLINK_OUTPUT_PATH="$PLINK_PATH/output/cov-as-phe_numeric_age"
 export PLINK_RESULT_PATH="$PLINK_PATH/results/cov-as-phe_numeric_age"
 
@@ -72,7 +72,7 @@ plink_execute=false
 plink_draw_fig=false
 covar_number=""
 
-# sbatch plink_full_BJ.sh -c -p -e --covar-number=4
+# sbatch plink_full_GZ.sh -c -p -e --covar-number=4
 
 while getopts "cpe-:" opt; do
   case $opt in
@@ -115,7 +115,7 @@ fi
 # Performing plink preprocessing
 if $plink_preprocess; then
   echo "Performing plink preprocessing..."
-  $PLINK_NEW_BIN --noweb --file $PLINK_OUTPUT_PATH/converted_genotyped --set-missing-var-ids @:# --recode --out $PLINK_PATH/converted_genotyped --allow-extra-chr --make-bed \
+  $PLINK_NEW_BIN --noweb --file $PLINK_OUTPUT_PATH/converted_genotyped --set-missing-var-ids @:# --recode --out $PLINK_OUTPUT_PATH/converted_genotyped --allow-extra-chr --make-bed \
   || { echo "Error: plink preprocessing failed."; exit 1; }
   echo "The plink preprocessing has been completed."
   echo "=============================="
@@ -131,7 +131,7 @@ if $plink_execute; then
     exit 1
   fi
   echo "Performing plink execution..."
-  $PLINK_NEW_BIN --bfile $PLINK_OUTPUT_PATH/converted_genotyped --linear --adjust --pheno $PLINK_PATH/phenotype_BJ_numeric.tsv --all-pheno --covar $PLINK_PATH/covariate_BJ.tsv --covar-number $covar_number --out $PLINK_RESULT_PATH/result --noweb --allow-extra-chr --allow-no-sex \
+  $PLINK_NEW_BIN --bfile $PLINK_OUTPUT_PATH/converted_genotyped --linear --adjust --pheno $PLINK_PATH/phenotype_GZ_numeric.tsv --all-pheno --covar $PLINK_PATH/covariate_GZ.tsv --covar-number $covar_number --out $PLINK_RESULT_PATH/result --noweb --allow-extra-chr --allow-no-sex \
   || { echo "Error: plink execution failed."; exit 1; }
   echo "The plink execution has been completed."
   echo "=============================="
