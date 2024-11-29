74f1fce5b9ae3f3e36ca63bb9c46bd20b4ded871
Lucas04-nhr <lucas04@foxmail.com>
2024-11-29 21:12:24 +0800

    Drop empty columns in adjResMerge.py before saving merged data

--- concat/adjResMerge.py
+++ concat/adjResMerge.py
@@ -27,6 +27,8 @@ def adj_merge (input_directory, output_file, nrows_threshold, sort):
   # Sort the data
   if sort is not None:
     merged_data = merged_data.sort_values(by=sort)
+  # Drop the empty columns
+  merged_data = merged_data.dropna(axis=1, how='all')
   # Save the merged data
   merged_data.to_csv(output_file, index=False, sep=',')
   # Clear the memory
