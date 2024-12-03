e1d7a4601332a544060fd517ff33fe79ecb9a835
Lucas04-nhr <lucas04@foxmail.com>
2024-12-03 14:31:47 +0800

    Update BJ sbatch script to modify array size and add GZ sbatch script for bacteria processing

--- replace/replace_bacteria_GZ.sh
+++ replace/replace_bacteria_GZ.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-#SBATCH --job-name=BJ_add_bacteria
-#SBATCH --output=./log/Beijing/BJ_log.%j.out
-#SBATCH --error=./log/Beijing/BJ_log.%j.err
+#SBATCH --job-name=GZ_add_bacteria
+#SBATCH --output=./log/Guangzhou/GZ_log.%j.out
+#SBATCH --error=./log/Guangzhou/GZ_log.%j.err
 #SBATCH --cpus-per-task=4
-#SBATCH --array=1-36%2
+#SBATCH --array=1-46%2
 #SBATCH --mem=24G
 
 # Initialize the environment
@@ -18,13 +18,13 @@ conda activate snp_analysis
 echo "The conda environment has been activated."
 echo "=============================="
 
-infile=($( cat BJ_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
+infile=($( cat GZ_sbatch.list | awk -v line=${SLURM_ARRAY_TASK_ID} '{if (NR==line) print $0}' ))
 
 # Extract the Phenotype Col Number
 pheno_col=$(echo $infile | grep -o -E '[0-9]+' | tail -n 1)
 echo "The phenotype column number is $pheno_col."
-python3 ./replace_bacteria.py -a -r --input-file=${infile} --bacteria-file="/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/bacteria_BJ.csv" \
-|| { echo "Error in add_bacteria.py"; exit 1; }
+python3 ./replace_bacteria.py -a -r --input-file=${infile} --bacteria-file="/mnt/raid6/bacphagenetwork/data/12_plink/Guangzhou/bacteria_GZ.csv" \
+|| { echo "Error in replace_bacteria.py"; exit 1; }
 echo "The bacteria have been added to the file."
 echo "=============================="
 echo "The script has finished running."
