3bc680bcd960443ef5b073d6dbcc15c886a59490
Lucas04-nhr <lucas04@foxmail.com>
2024-12-04 14:48:13 +0800

    Refactor directory variable assignments in draw_plot_BJ.sh for improved readability

--- draw_manhattan_plot/draw_plot_BJ.sh
+++ draw_manhattan_plot/draw_plot_BJ.sh
@@ -17,17 +17,17 @@ echo "The conda environment has been activated."
 echo "=============================="
 
 echo "Processing the Manhattan plot of Beijing..."
-export INPUT_DIR = "/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/replaced/bac_age"
-export OUTPUT_DIR = "/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/merged/manhattan_plot"
+export INPUT_DIR="/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/replaced/bac_age"
+export OUTPUT_DIR="/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/merged/manhattan_plot"
 
-echo "The input directory is /mnt/raid6/bacphagenetwork/data/12_plink/Beijing/replaced/bac_age."
-echo "The output directory is /mnt/raid6/bacphagenetwork/data/12_plink/Beijing/merged/manhattan_plot."
+echo "The input directory is $INPUT_DIR."
+echo "The output directory is $OUTPUT_DIR."
 
 echo "-----------------------------"
 
 echo "Drawing the Manhattan plot..."
 
-Rscript ./draw_plot.r --input-directory="/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/replaced/bac_age" --output-directory="/mnt/raid6/bacphagenetwork/data/12_plink/Beijing/merged/manhattan_plot" \
+Rscript ./draw_plot.r --input-directory=$INPUT_DIR --output-directory=$OUTPUT_DIR \
 || { echo "Error in draw_plot.r"; exit 1; }
 
 
