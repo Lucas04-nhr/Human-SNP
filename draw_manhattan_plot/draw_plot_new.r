ea10dcce01f6681259eb073395bcd9c1e3914995
Lucas04-nhr <lucas04@foxmail.com>
2024-12-16 19:55:52 +0800

    Add option to use saved data in Manhattan plot scripts

--- draw_manhattan_plot/draw_plot_new.r
+++ draw_manhattan_plot/draw_plot_new.r
@@ -15,7 +15,9 @@ option_list <- list(
   make_option(c("--input-directory"), type = "character", default = ".",
               help = "Input directory", metavar = "character"),
   make_option(c("--output-directory"), type = "character", default = ".",
-              help = "Output directory", metavar = "character")
+              help = "Output directory", metavar = "character"),
+  make_option(c("--use-saved-data"), type = "logical", default = TRUE,
+              help = "Use saved RData if available", metavar = "logical")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
@@ -80,67 +82,71 @@ print(output_file)
 
 known_columns <- c("CHR", "SNP", "UNADJ", "GC", "BONF", "HOLM", "SIDAK_SS", "SIDAK_SD", "FDR_BH", "FDR_BY", "Bacteria") # nolint
 
-df_list <- lapply(input_files, function(file) {
-  print(paste("Processing file", file))
-  df <- read.table(file, header = TRUE, row.names = NULL, sep = ",")
-  df <- df[, known_columns[known_columns %in% colnames(df)]]
-  df$SNP <- as.character(df$SNP)  # Ensure SNP column is character
-  print(paste(file, "has", nrow(df), "rows and", ncol(df), "columns"))
-  print(paste("The", file, "has been processed."))
-  print(" ")
-  return(df)
-})
+df_file <- file.path(output_directory, "df.RData")
 
-print("Head of the processed data:")
-print(head(df_list[[1]]))
+if (file.exists(df_file) && opt$use_saved_data) {
+  print("Loading existing df.RData...")
+  load(df_file)
+} else {
+  df_list <- lapply(input_files, function(file) {
+    print(paste("Processing file", file))
+    df <- read.table(file, header = TRUE, row.names = NULL, sep = ",")
+    df <- df[, known_columns[known_columns %in% colnames(df)]]
+    df$SNP <- as.character(df$SNP)  # Ensure SNP column is character
+    print(paste(file, "has", nrow(df), "rows and", ncol(df), "columns"))
+    print(paste("The", file, "has been processed."))
+    print(" ")
+    return(df)
+  })
 
-print("Processing data...")
+  print("Head of the processed data:")
+  print(head(df_list[[1]]))
 
-df_all <- bind_rows(df_list)
+  print("Processing data...")
 
-df_all$BP <- sapply(df_all$SNP, function(x) {
-  parts <- strsplit(x, ":")
-  return(parts[[1]][2])  # 返回分割后的第二部分
-})
+  df_all <- bind_rows(df_list)
 
-print("Filtering data...")
+  df_all$BP <- sapply(df_all$SNP, function(x) {
+    parts <- strsplit(x, ":")
+    return(parts[[1]][2])  # 返回分割后的第二部分
+  })
 
-# Filter out SNPs with -log10(BONF) > 10, as BONF < 1e-10
-df_all <- df_all %>%
-  filter(df_all$BONF < 5e-8)
+  print("Filtering data...")
 
-print("Sorting data...")
+  # Filter out SNPs with -log10(BONF) > 10, as BONF < 1e-10
+  df_all <- df_all %>%
+    filter(df_all$BONF < 5e-8)
 
-df_all$CHR[!df_all$CHR %in% c(1:22, 23, 26)] <- 30
+  print("Sorting data...")
 
-df_all$CHR<-as.numeric(df_all$CHR)
-df_all$BP<-as.numeric(df_all$BP)
+  df_all$CHR[!df_all$CHR %in% c(1:22, 23, 26)] <- 30
 
-sorted_df_all <- df_all %>%
-  arrange(CHR, BP)
-sorted_df <- sorted_df_all[, c("CHR", "BP", "SNP", "BONF", "Bacteria")]
+  df_all$CHR <- as.numeric(df_all$CHR)
+  df_all$BP <- as.numeric(df_all$BP)
 
-name <- unique(sorted_df_all$CHR)
+  sorted_df_all <- df_all %>%
+    arrange(CHR, BP)
+  sorted_df <- sorted_df_all[, c("CHR", "BP", "SNP", "BONF", "Bacteria")]
 
-# 将CHR中的23替换为X, 26替换为Y, 其他替换为Others
-# old_elements <- c("23", "26", "30") # nolint
-# new_elements <- c("X", "Y", "Others") # nolint
+  name <- unique(sorted_df_all$CHR)
 
-#定义df
-df <- sorted_df_all
+  #定义df
+  df <- sorted_df_all
 
-# df$CHR <- ifelse(df$CHR %in% old_elements, new_elements, df$CHR) # nolint
+  print("Calculating rank...")
 
-print("Calculating rank...")
+  df$Bacteria <- as.character(df$Bacteria)
 
-df$Bacteria <- as.character(df$Bacteria)
+  df <- df %>%
+    group_by(Bacteria) %>%
+    mutate(rank_p = rank(BONF, ties.method = "first")) %>% # 对P进行排名
+    mutate(Bacteria_new = if_else(rank_p == 1, Bacteria, NA_character_)) %>% # 保留P最小的行的Bacteria值，其他变成NA # nolint
+    ungroup() %>%
+    select(-rank_p)
 
-df <- df %>%
-  group_by(Bacteria) %>%
-  mutate(rank_p = rank(BONF, ties.method = "first")) %>% # 对P进行排名
-  mutate(Bacteria_new = if_else(rank_p == 1, Bacteria, NA_character_)) %>% # 保留P最小的行的Bacteria值，其他变成NA # nolint
-  ungroup() %>%
-  select(-rank_p)
+  print("Saving data...")
+  save(df, file = df_file)
+}
 
 print("Drawing plot...")
 
