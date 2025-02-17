68ce08bba3c3591458cb09b5fc924918f0ffbe7d
ErikaSirius <144027085+ErikaSirius@users.noreply.github.com>
2025-02-17 17:57:32 +0800

    Update run_annovar.sh
    annovar for full variants

--- geneAnnotation/run_annovar.sh
+++ geneAnnotation/run_annovar.sh
@@ -19,10 +19,12 @@ echo "The conda environment has been activated."
 # 设置变量
 ANNOVAR_DIR="/mnt/raid6/bacphagenetwork/tools/annovar"          # ANNVAR 的安装路径
 DB_DIR="${ANNOVAR_DIR}/humandb"         # 数据库存放路径
-FILE_DIR_PREFIX="/mnt/raid6/bacphagenetwork/data/13_ANNOVAR/Guangzhou"   # 文件存放路径前缀
-INPUT_FILE="${FILE_DIR_PREFIX}/gz_for_annovar.csv"          # 输入文件
-OUTPUT_PREFIX="${FILE_DIR_PREFIX}/results/gz_annotated_results"       # 输出文件前缀
-BUILD="hg38"                            # 基因组版本
+VCF_FILE="/mnt/raid6/bacphagenetwork/data/10_ApplyVQSR/Full/joint_genotyped.filtered.vcf.gz"                 # 输入文件
+INPUT_FILE="/mnt/raid6/bacphagenetwork/data/13_ANNOVAR/full_input.txt"          # 输入文件
+OUTPUT_PREFIX="/mnt/raid6/bacphagenetwork/data/13_ANNOVAR/result/full_annotated_results"       # 输出文件前缀
+BUILD="hg38"   
+
+perl "${ANNOVAR_DIR}/convert2annovar.pl" -format vcf4 "${VCF_FILE}" > "${INPUT_FILE}"
 
 # 运行注释命令
 echo ">>> Start annotating mutations..."
