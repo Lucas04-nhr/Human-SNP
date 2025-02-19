b64579915dad749cbef08a7659721c2f77d34e26
Lucas04-nhr <lucas04@foxmail.com>
2025-02-19 17:05:10 +0800

    Add sbatch script for merging Full dataset files with error handling

---
+++ concat/adjResMerge_Full.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+#SBATCH --job-name=BJ_merge
+#SBATCH --output=./log/BJ_merge_log.%j.out
+#SBATCH --error=./log/BJ_merge_log.%j.err
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+
+# Initialize the environment
+echo "Initializing the environment..."
+echo "=============================="
+echo ""
+
+# Load conda
+echo "Activating conda environment..."
+source /home/bacphagenetwork/.bashrc
+conda activate snp_analysis
+echo "The conda environment has been activated."
+echo "=============================="
+
+# Merge the files
+echo "Merging the files of Full dataset..."
+python3 ./adjResMerge.py --input-directory=/mnt/raid6/bacphagenetwork/data/12_plink/Full/modified/bac_age --nrows-threshold=100 \
+|| { echo "Error during the merging process."; exit 1; }
+echo "The files have been merged."
+
+echo "=============================="
+echo "The script has finished running."
