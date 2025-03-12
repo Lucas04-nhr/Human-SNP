f4d9bc20179f98ee07252687025aeb093b8e7f37
Lucas <lucas04@foxmail.com>
2025-03-12 20:20:24 +0800

    refactor: Remove draw_plot_BJ.sh and update draw_plot_full.sh for Beijing processing

--- draw_manhattan_plot/draw_plot_full.sh
+++ draw_manhattan_plot/draw_plot_full.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-#SBATCH --job-name=Full_draw
-#SBATCH --output=./Full_log.%j.out
-#SBATCH --error=./Full_log.%j.err
+#SBATCH --job-name=BJ_draw
+#SBATCH --output=./BJ_log.%j.out
+#SBATCH --error=./BJ_log.%j.err
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=256G
+#SBATCH --mem=64G
 
 # Initialize the environment
 echo "Initializing the environment..."
@@ -16,8 +16,8 @@ conda activate snp_analysis
 echo "The conda environment has been activated."
 echo "=============================="
 
-echo "Processing the Manhattan plot of Full..."
-export INPUT_DIR="/mnt/raid6/bacphagenetwork/data/12_plink/Full/replaced/bac_age"
+echo "Processing the Manhattan plot of Beijing..."
+export INPUT_DIR="/mnt/raid6/bacphagenetwork/data/12_plink/Full/modified/bac_age/"
 export OUTPUT_DIR="/mnt/raid6/bacphagenetwork/data/12_plink/Full/merged/manhattan_plot"
 
 echo "The input directory is $INPUT_DIR."
