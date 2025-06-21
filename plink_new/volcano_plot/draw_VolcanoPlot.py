3e658a8d311a2614b39feadae8f5af068de7392a
Lucas <lucas04@foxmail.com>
2025-06-21 16:07:03 +0800

    chore: Adjust indentation and improve readability in `draw_VolcanoPlot.py`

--- plink_new/volcano_plot/draw_VolcanoPlot.py
+++ plink_new/volcano_plot/draw_VolcanoPlot.py
@@ -14,14 +14,14 @@ args = parser.parse_args()
 input_file = args.input_file # /mnt/raid6/bacphagenetwork/data/12_plink_Full/converted/*.assoc.linear
 output_directory = args.output_directory # /mnt/raid6/bacphagenetwork/data/12_plink_Full/volcano_plot
 if not os.path.exists(output_directory):
-    os.makedirs(output_directory)
+  os.makedirs(output_directory)
 
 # 读取表型文件
 try:
-    df = pd.read_csv(input_file, sep=',', low_memory=False)
-    print(f"Input file loaded with {len(df)} rows and {len(df.columns)} columns.")
+  df = pd.read_csv(input_file, sep=',', low_memory=False)
+  print(f"Input file loaded with {len(df)} rows and {len(df.columns)} columns.")
 except FileNotFoundError as fnfe:
-    raise FileNotFoundError(f"Error while reading the input file: {fnfe}")
+  raise FileNotFoundError(f"Error while reading the input file: {fnfe}")
 
 # 构建输出文件名
 input_filename = os.path.basename(input_file)
@@ -33,12 +33,12 @@ top_csv_file = os.path.join(output_directory, f"00_top_snps.csv")
 
 # 确认是否已存在 top_csv_file
 if os.path.exists(top_csv_file):
-    print(f"Top SNPs file already exists: {top_csv_file}")
-    top_output = pd.read_csv(top_csv_file)
+  print(f"Top SNPs file already exists: {top_csv_file}")
+  top_output = pd.read_csv(top_csv_file)
 else:
-    print(f"Top SNPs file does not exist, creating new one: {top_csv_file}")
-    # CHR,SNP,BP,A1,TEST,NMISS,BETA,STAT,P,Bacterium
-    top_output = pd.DataFrame(columns=['CHR', 'SNP', 'BP', 'A1', 'TEST', 'NMISS', 'BETA', 'STAT', 'P', 'Bacterium'])
+  print(f"Top SNPs file does not exist, creating new one: {top_csv_file}")
+  # CHR,SNP,BP,A1,TEST,NMISS,BETA,STAT,P,Bacterium
+  top_output = pd.DataFrame(columns=['CHR', 'SNP', 'BP', 'A1', 'TEST', 'NMISS', 'BETA', 'STAT', 'P', 'Bacterium'])
 
 # 数据预处理
 # sortable_columns = ['BP', 'NMISS', 'BETA', 'SE', 'R2', 'SIDAK_SD', 'T', 'P']
@@ -60,7 +60,7 @@ plt.figure(figsize=(10, 6), dpi=300)
 plt.scatter(df[effective_column], df['neg_log_p'], alpha=0.5)
 # 标识显著SNP
 for _, row in top_snps.iterrows():
-    plt.text(row[effective_column], row['neg_log_p'], row['SNP'], fontsize=9)
+  plt.text(row[effective_column], row['neg_log_p'], row['SNP'], fontsize=9)
 plt.axhline(y=-np.log10(0.05), color='r', linestyle='--')
 plt.title(f'Volcano Plot for {pheno_name}')
 plt.xlabel(effective_column)
